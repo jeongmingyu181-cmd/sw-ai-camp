2b68d2016c84e64e9023b3211b4025720f3ce3aa
jeongmingyu <jeongmingyu181@gmail.com>
2025-10-24 19:57:46 +0900

    wow

--- day1.py
+++ day1.py
@@ -1,13 +1,1 @@
-from tkinter import *
-
-def click():
-    print("Hello World")
-
-win = Tk()
-win.geometry("800x1000")
-
-btn = Button()
-btn.config(command = click)
-btn.pack()
-
-mainloop()
+print("Hello World")
